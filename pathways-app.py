bf1074e702d72d488db74d95b1c2a3cb422e2125
Alex Kalderimis <alex@flymine.org>
2011-10-10 14:54:49 +0100

    Fixed a couple of mostly spelling errors.

--- pathways-app.py
+++ pathways-app.py
@@ -14,59 +14,60 @@ pd = PathwayDemo()
 
 @app.route('/')
 def index():
-	return redirect(url_for('search'))
+    return redirect(url_for('search'))
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-	if request.method == 'POST':
-		symbol = request.form['symbol']
-		org = request.form['organism']
-		if symbol:
-			genes = pd.find_gene(symbol, org_util.get_name(org))
-			if len(genes) == 1:
-				symbol = genes[0][0]
-				return redirect(url_for('view', symbol=symbol, organism=org))
-		flash('Could not find gene: ' + symbol + ' in organism ' + org)
-		return redirect(url_for('search'))
-	else:
-		orgs = org_util.get_abbrevs()
-		return render_template('search.html', organisms=orgs)
+    if request.method == 'POST':
+        symbol = request.form['symbol']
+        org = request.form['organism']
+        if symbol:
+            genes = pd.find_gene(symbol, org_util.get_name(org))
+            if len(genes) == 1:
+                symbol = genes[0][0]
+                return redirect(url_for('view', symbol=symbol, organism=org))
+        flash('Could not find gene: ' + symbol + ' in organism ' + org)
+        return redirect(url_for('search'))
+    else:
+        orgs = org_util.get_abbrevs()
+        return render_template('search.html', organisms=orgs)
 
 @app.route('/view/<organism>/<symbol>')
 def view(organism, symbol):
-	# if we came from the search page then we know the symbol has been
-	# found already, othwerise was entered in the URL so do search
-	if not request.referrer or not request.referrer.endswith('search'):
-		genes = pd.find_gene(symbol, org_util.get_name(organism))
-		if len(genes) == 1:
-			symbol = genes[0][0]
-		else:
-			flash('Could not find gene: ' + symbol + ' in organism ' + organism)
-			return redirect(url_for('search'))
+    # if we came from the search page then we know the symbol has been
+    # found already, othwerise was entered in the URL so do search
+    if not request.referrer or not request.referrer.endswith('search'):
+        genes = pd.find_gene(symbol, org_util.get_name(organism))
+        if len(genes) == 1:
+            symbol = genes[0][0]
+        else:
+            flash('Could not find gene: ' + symbol + ' in organism ' + organism)
+            return redirect(url_for('search'))
 
-	org_full_name = org_util.get_name(organism)
-	homologs = pd.get_homologs_for_gene(symbol, org_full_name)
+    org_full_name = org_util.get_name(organism)
+    homologs = pd.get_homologs_for_gene(symbol, org_full_name)
 
-	# include starting gene in search
-	all_orgs = dict(homologs)
-	if not org_full_name in all_orgs:
-		all_orgs[org_full_name] = {}
-	all_orgs[org_full_name][symbol] = None
+    # include starting gene in search
+    all_orgs = dict(homologs)
+    if not org_full_name in all_orgs:
+        all_orgs[org_full_name] = {}
+    all_orgs[org_full_name][symbol] = None
 
-	# fetch pathways 
-	pathways = []
-	for org in all_orgs:
-		for gene in all_orgs[org]:
-			for gene_pathways in pd.get_pathways(gene, org):
-				pathways.append(gene_pathways)
+    # fetch pathways 
 
-	# re-organise for display:  {pathway:  {organisms: [genes]}}
-	table = defaultdict(lambda: defaultdict(list))
-	for row in pathways:
-		table[row[0]][row[1]].append(row[2])
-	return render_template('view.html', symbol=symbol, organism=org_full_name, org_abbrev=organism, orgs=all_orgs, homologs=homologs, table=table)
-	
+    pathways = []
+    for org in all_orgs:
+        for gene in all_orgs[org]:
+            for pws in pd.get_pathways(gene, org):
+                pathways.append(pws)
+    
+    # re-organise for display:  {pathway:  {organisms: [genes]}}
+    table = defaultdict(lambda: defaultdict(list))
+    for row in pathways:
+        table[row[0]][row[1]].append(row[2])
+    return render_template('view.html', symbol=symbol, organism=org_full_name, org_abbrev=organism, orgs=all_orgs, homologs=homologs, table=table)
+    
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=False)
+    app.run('0.0.0.0', debug=True)
 
--- pathways.py
+++ pathways.py
@@ -7,86 +7,90 @@ from itertools import groupby, ifilter
 import org_util
 
 class PathwayDemo(object):
-	service_urls = {'Drosophila melanogaster': "http://www.flymine.org/query/service",
-			'Saccharomyces cerevisiae': "http://yeastmine.yeastgenome.org/yeastmine/service",
-			'Rattus norvegicus': "http://ratmine.mcw.edu/ratmine/service",
-			'Mus musculus': "http://metabolicmine.org/test/service",
-			'Homo sapiens': "http://metabolicmine.org/test/service",
-	}
+    service_urls = {'Drosophila melanogaster': "http://www.flymine.org/query/service",
+            'Saccharomyces cerevisiae': "http://yeastmine.yeastgenome.org/yeastmine/service",
+            'Rattus norvegicus': "http://ratmine.mcw.edu/ratmine/service",
+            'Mus musculus': "http://metabolicmine.org/test/service",
+            'Homo sapiens': "http://metabolicmine.org/test/service",
+    }
 
-	def __init__(self):
-		self.services = {}
-		for (name, service_url) in self.service_urls.items():
-			try:
-				self.services[name] = Service(service_url)
-				print "Connected to %s" % service_url
-			except ServiceError:
-				print "Failed to initialise: %s" % service_url
-		 
+    def __init__(self):
+        self.services = {}
+        for (name, service_url) in self.service_urls.items():
+            try:
+                self.services[name] = Service(service_url)
+                print "Connected to %s" % service_url
+            except ServiceError as e:
+                print "Failed to initialise: %s, %s" % (service_url, e)
+         
 
-	# returns a list of lists
-	def find_gene(self, symbol, org_name):
-		service = self.services[org_name]
+    # returns a list of lists
+    def find_gene(self, symbol, org_name):
+        service = self.services[org_name]
 
-		query = service.new_query("Gene")\	
-			       .select("symbol", "primaryIdentifier", "name", "organism.name")\
-		               .where("Gene", "LOOKUP", symbol)\
-		               .where("organism.name", "=", org_name)
+        query = service.new_query("Gene")\
+                       .select("symbol", "primaryIdentifier", "name", "organism.name")\
+                       .where("Gene", "LOOKUP", symbol)\
+                       .where("organism.name", "=", org_name)
 
-		return [row.to_l() for row in query.rows()]
-				
+        return [row.to_l() for row in query.rows()]
+                
 
-	def get_homologs_for_gene(self, symbol, org_name):
-		# always use FlyMine for querying homologs
-		
-		h_sym = "homologues.homologue.symbol"
-		h_org = "homologues.homologue.organism.name"
-		h_ds  = "homologues.dataSets.name"
-		
-		service = self.services["Drosophila melanogaster"]
-    	
-		query = service.new_query("Gene")\
-		               .select(h_org, h_sym, h_ds)\
-		               .where(h_org, "ONE OF", org_util.get_names())\
-		               .where("organism.name", "=", org_name)\
-		               .where("symbol", "=", symbol)\
-		               .where(h_sym, "IS NOT NULL")
+    def get_homologs_for_gene(self, symbol, org_name):
+        # always use FlyMine for querying homologs
+        
+        h_sym = "homologues.homologue.symbol"
+        h_org = "homologues.homologue.organism.name"
+        h_ds  = "homologues.dataSets.name"
+        
+        service = self.services["Drosophila melanogaster"]
+        
+        query = service.new_query("Gene")\
+                       .select(h_org, h_sym, h_ds)\
+                       .where(h_org, "ONE OF", org_util.get_names())\
+                       .where("organism.name", "=", org_name)\
+                       .where("symbol", "=", symbol)\
+                       .where(h_sym, "IS NOT NULL")
 
-		homologs = defaultdict(dict)		
-		
-		for org, g1 in groupby(query.rows(), lambda x: x[h_org]):
-			for sym, g2 in groupby(g1, lambda x: x[h_sym]):
-				homlogues[org][sym] = [r[h_ds] for r in g2]
-		return homologs
+        homologs = defaultdict(dict)        
+        
+        for org, g1 in groupby(query.rows(), lambda x: x[h_org]):
+            for sym, g2 in groupby(g1, lambda x: x[h_sym]):
+                homologs[org][sym] = [r[h_ds] for r in g2]
+        return homologs
  
-	def get_pathways(self, symbol, org_name):
-		service = self.services[org_name]
-		sym = "symbol"
-		pw = "pathways.name"
-		ds = 'Gene.pathways.dataSets.name'
-		org = "organism.name"
-		query = service.new_query("Gene").select(sym, org, pw)
-	
-		# YeastMine doesn't have pathway.dataSets, check model first
-		if self.is_path_in_model(org_name, ds):
-			query.add_view(ds)
-			query.add_join('Gene.pathways.dataSets', 'OUTER')
+    def get_pathways(self, symbol, org_name):
+        
+        service = self.services[org_name]
+        sym = "symbol"
+        pw = "pathways.name"
+        ds = 'Gene.pathways.dataSets.name'
+        org = "organism.name"
+        query = service.new_query("Gene").select(sym, org, pw)
 
-		query.add_sort_order(pw, 'asc')
-		query.add_constraint(sym, '=', symbol)
-		query.add_constraint(org, "=", org_name)
-	
-	        # Return a list of triples
-		return [(r[pw] + " (%s)" % r[ds] if len(r) == 3 else r[pw], r[org], r[sym]) for r in query.rows()]
+        if org_name == "Rattus norvegicus":
+            return []
+    
+        # YeastMine doesn't have pathway.dataSets, check model first
+        if self.is_path_in_model(org_name, ds):
+            query.add_view(ds)
+            query.add_join('Gene.pathways.dataSets', 'OUTER')
 
-	def is_path_in_model(self, org, path):
-		service = self.services[org]	
-		try:
-			service.model.validate_path(path)
-		except:
-			return False
-		return True
+        query.add_sort_order(pw, 'asc')
+        query.add_constraint(sym, '=', symbol)
+        query.add_constraint(org, "=", org_name)
+    
+        # Return a list of triples
+        return [[r[pw] + " (%s)" % r[ds] if len(r) == 4 else r[pw], r[org], r[sym]] for r in query.rows()]
 
-	def strip_suffix(self, dataset):
-		if dataset.endswith('data set'):
-			return dataset[0:dataset.find('data set')]
+    def is_path_in_model(self, org, path):
+        service = self.services[org]    
+        try:
+            service.model.validate_path(path)
+        except:
+            return False
+        return True
+
+    def strip_suffix(self, dataset):
+        if dataset.endswith('data set'):
+            return dataset[0:dataset.find('data set')]
